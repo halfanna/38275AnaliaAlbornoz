80a7841c677459ae7c69c4f628621e39112c1e30
anna <anafernandaalbornoz@gmail.com>
2022-08-10 22:24:02 -0300

    agregue cambios en js

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
-alert("Bienvenidos , elija los costos")
+alert("Bienvenidos!!! Elija los costos.")
 
 let articuloA = Number(prompt("Ingresa el costo del producto \n1. REMERA =2000 \n2. BUZO =2500 "))
 let articuloB = Number(prompt("Ingresa el costo del producto \n1. PANTALON = 4500 \n2. CAMPERA = 3500 "))
-let agregar = prompt("Colocar +  para agregar al carrito")
+let agregar = prompt("Colocar + O - para agregar al carrito")
+
+
+function carrito(art1,art2,operador){
+
+    switch(operador){
+    
+    case"+":
+    
+    return alert(art1+art2);
+    
+    case"-":
+    
+    return alert(art1-art2);
+    
+    default:
+    
+    alert("Agrega un producto")
+    
+    break;
+    
+    }
+    
+    }
+
 
 do {
-   agregar =prompt("Ingresa + para continuar")
+    agregar =prompt("Ingresa + O - para continuar")
    
-} while ( agregar != "+");
+}while ((agregar !="+" ) && ( agregar !="-"));
+ 
+carrito(articuloA,articuloB,agregar);
 
-
-const calcularPrecio = (precioArticuloUno,precioArticuloDos) => precioArticuloUno+precioArticuloDos
-
+const calcularPrecio = (precioArticuloUno,PrecioArticuloDos)=> precioArticuloUno+PrecioArticuloDos
 const tarjeta = compraFinal => compraFinal * 1.21
 
+
+let compraEfectivo = calcularPrecio(articuloA,articuloB)
+let compraTarjeta =tarjeta(compraEfectivo)
 let medioDePago =prompt("Elije medio de pago : \n1. EFECTIVO \n2. TARJETA ")
-let compra = calcularPrecio(articuloA,articuloB)
-let compraTarjeta =tarjeta(compra)
+
+
 
 if (medioDePago !=" ") {
       if ( (medioDePago == "EFECTIVO") ||(medioDePago == "efectivo") ) {
-         alert("Precio con descuento en efectivo ="+compra +  "\n Finalizaste tu compra.")
+         alert("Precio con descuento en efectivo ="+compraEfectivo +  "\n Finalizaste tu compra.")
       }
+     
+     
         else if ((medioDePago == "TARJETA")||(medioDePago == "tarjeta") ){
         alert("Precio en tarjeta con IVA = "+compraTarjeta + "\n Finalizaste tu compra.")
     } 
@@ -31,7 +60,6 @@ if (medioDePago !=" ") {
     }
     
 }
-   
 
     
   
